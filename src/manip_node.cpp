09faa9d80d90ab795026bb20889578f619e3a07f
Amal Pavithran <amalpavithranmp@gmail.com>
2019-10-29 22:32:09 +0530

    Added min & max params

--- src/manip_node.cpp
+++ src/manip_node.cpp
@@ -10,33 +10,35 @@ class pid_model {
     ros::Subscriber link_in;
     ros::Subscriber link_set;
     ros::Publisher link_out;
-    double kp,ki,kd,errSum=0,lastErr=0,input,setpoint;
+    double kp,ki,kd,errSum=0,lastErr=0,input,setpoint,high,low;
     double compute(double Setpoint,double Input){
         double error = Setpoint - Input;
         errSum += error;
         double dErr = (error - lastErr);
         lastErr = error;
         double value = (kp*error + ki*(errSum) + kd*dErr);
-        if(errSum>255){
+        if(errSum>high){
             errSum=0;
         }
-        if(value>=255)
-            return 255;
-        else if(value<=-255)
-            return -255;
+        if(value>=high)
+            return high;
+        else if(value<=low)
+            return low;
         else
             return value;
     }
     
     public:
-    pid_model(double p,double i,double d,string topic,ros::NodeHandle n){
+    pid_model(double p,double i,double d,double max,double min,string topic,ros::NodeHandle n){
         kp = p;
         ki = i;
         kd = d;
+        high = max;
+        low = min;        
         name=topic;
-       link_in = n.subscribe(topic+"_in",1,&pid_model::get_in,this);
-       link_set = n.subscribe(topic+"_set",1,&pid_model::get_set,this);
-       link_out = n.advertise<std_msgs::Int32>(name+"_out",1);
+        link_in = n.subscribe(topic+"_in",1,&pid_model::get_in,this);
+        link_set = n.subscribe(topic+"_set",1,&pid_model::get_set,this);
+        link_out = n.advertise<std_msgs::Int32>(name+"_out",1);
     }
     void get_in(const std_msgs::Int32 msg){
         input = msg.data;
@@ -49,7 +51,6 @@ class pid_model {
         setpoint = msg.data;
         int val = compute(setpoint,input);
         std_msgs::Int32 opt;
-        ROS_INFO("Running");
         opt.data = val;
         link_out.publish(opt);
     }
@@ -59,15 +60,19 @@ class pid_model {
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "link1");
-    double kp,ki,kd;
+    double kp,ki,kd,max,min;
     string nam;
     ros::NodeHandle n("~");
     n.getParam("kp",kp);
     n.getParam("ki",ki);
     n.getParam("kd",kd);
+    n.getParam("max",max);
+    n.getParam("min",min);
     n.param<std::string>("nam",nam,"link1");
     string name = ros::this_node::getName();
-    pid_model link1(kp,ki,kd,name,n);
+    ROS_INFO("INITIALIZED");
+    pid_model link1(kp,ki,kd,max,min,name,n);
+    ROS_INFO("OBJECT CREATED");
     while(ros::ok()){
       ros::spinOnce();  
     }
